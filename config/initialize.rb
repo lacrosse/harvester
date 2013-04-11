a969d66ca036a47c71a99219321c022e9c283233
Alexander <skiba-alex@ya.ru>
2013-04-11 17:03:09 +0700

    Change initialization order

--- config/initialize.rb
+++ config/initialize.rb
@@ -4,6 +4,6 @@ APP_PATH = File.expand_path('../../', __FILE__) + '/'
 
 CREDENTIALS = YAML.load_file(APP_PATH + 'config/credentials.yml')
 
-require APP_PATH + 'lib/harvest.rb'
 require APP_PATH + 'lib/google.rb'
+require APP_PATH + 'lib/harvest.rb'
 require APP_PATH + 'lib/qbo.rb'
